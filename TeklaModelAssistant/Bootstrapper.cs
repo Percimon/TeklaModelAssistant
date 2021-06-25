8f9788145690d414425c2dd4e3e5562b40ff439f
Vladislav Lakeev <chilli.willi.930@gmail.com>
2021-06-25 23:43:45 +0300

    Project created and Caliburn structure added

---
+++ TeklaModelAssistant/Bootstrapper.cs
@@ -0,0 +1,32 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using TeklaModelAssistant.ViewModels;
+
+namespace TeklaModelAssistant
+{
+    class Bootstrapper : BootstrapperBase
+    {
+        private SimpleContainer simpleContainer = new SimpleContainer();
+
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void Configure()
+        {
+            simpleContainer.Singleton<ShellViewModel>();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+    }
+}
