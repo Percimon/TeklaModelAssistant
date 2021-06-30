e3fe7a6e0960a68b5f14a5bbf21e382abb1ec455
Vladislav Lakeev <chilli.willi.930@gmail.com>
2021-06-30 23:06:56 +0300

    ObjectData V and VM updated

---
+++ TeklaModelAssistant/ViewModels/ObjectAttributesViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Caliburn.Micro;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TeklaModelAssistant.ViewModels
+{
+    class ObjectAttributesViewModel
+    {
+        private IEventAggregator eventAggregator;
+
+        public ObjectAttributesViewModel(IEventAggregator eventAggregator)
+        {
+            this.eventAggregator = eventAggregator;
+        }
+    }
+}
--- TeklaModelAssistant/ViewModels/ObjectDataViewModel.cs
+++ TeklaModelAssistant/ViewModels/ObjectDataViewModel.cs
@@ -10,45 +10,17 @@ using TeklaModelAssistant.Infrastructure;
 
 namespace TeklaModelAssistant.ViewModels
 {
-    class ObjectDataViewModel : PropertyChangedBase, IHandle<TeklaModelObjectProvider>
+    class ObjectDataViewModel
     {
         private IEventAggregator eventAggregator;
-
-        private string guid;
-        public string GUID
-        {
-            get { return guid; }
-            set { guid = value; NotifyOfPropertyChange(() => GUID); }
-        }
-
-        private string type;
-
-        public string Type
-        {
-            get { return type; }
-            set { type = value; NotifyOfPropertyChange(() => Type); }
-        }
-
+        public ObjectIdentificationViewModel ObjectIdentificationViewModel { get; set; }
+        public ObjectAttributesViewModel ObjectAttributesViewModel { get; set; }
 
         public ObjectDataViewModel(IEventAggregator eventAggregator)
         {
             this.eventAggregator = eventAggregator;
-            this.eventAggregator.SubscribeOnUIThread(this);
-        }
-
-        public System.Threading.Tasks.Task HandleAsync(TeklaModelObjectProvider message, CancellationToken cancellationToken)
-        {
-            ModelObject mObject = message.TeklaModelObject;
-            if(mObject != null)
-            {
-                GUID = $"{mObject.Identifier.GUID}";
-                Type = $"{mObject.GetType()}".Replace("Tekla.Structures.Model.","");
-            }
-            else
-            {
-                GUID = Type = "Empty";
-            }
-            return System.Threading.Tasks.Task.CompletedTask;
+            ObjectIdentificationViewModel = new ObjectIdentificationViewModel(eventAggregator);
+            ObjectAttributesViewModel = new ObjectAttributesViewModel(eventAggregator);
         }
     }
 }
