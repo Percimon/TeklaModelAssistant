a511636c6b6a290e7977e892813d67d9d566ce72
Vladislav Lakeev <chilli.willi.930@gmail.com>
2021-07-04 13:29:51 +0300

    part wrapper attributes added

--- TeklaModelAssistant/Models/PartWrapper.cs
+++ TeklaModelAssistant/Models/PartWrapper.cs
@@ -11,21 +11,88 @@ namespace TeklaModelAssistant.Models
     class PartWrapper : PropertyChangedBase
     {
         #region Name
-        private string name;
+        private string _name;
 
         public string Name
         {
-            get { return name; }
-            set { name = value; NotifyOfPropertyChange(()=>Name); _part.Name = name; CommitChanges(); }
+            get { return _name; }
+            set
+            {
+                _name = value; 
+                NotifyOfPropertyChange(() => Name);
+                _part.Name = _name;
+                CommitChanges();
+            }
         }
 
         #endregion
 
+        #region Class
+
+        private int _class;
+
+        public int Class
+        {
+            get { return _class; }
+            set
+            {
+                _class = value; 
+                NotifyOfPropertyChange(() => Class); 
+                _part.Class = $"{Class}";
+                CommitChanges();
+            }
+        }
+
+
+        #endregion
+
+        #region Profile
+
+        private string _profile;
+
+        public string Profile
+        {
+            get { return _profile; }
+            set 
+            { 
+                _profile = value; 
+                NotifyOfPropertyChange(() => Profile);
+                _part.Profile.ProfileString = Profile; 
+                CommitChanges(); 
+            }
+        }
+
+
+        #endregion
+
+        #region Material
+
+        private string _material;
+
+        public string Material
+        {
+            get { return _material; }
+            set 
+            { 
+                _material = value; 
+                NotifyOfPropertyChange(() => Material);
+                _part.Material.MaterialString = Material; 
+                CommitChanges(); 
+            }
+        }
+
+
+        #endregion
+
+      
         private Part _part;
         public PartWrapper(Part part)
         {
             _part = part;
             Name = part.Name;
+            Class = Int32.Parse(part.Class);
+            Profile = part.Profile.ProfileString;
+            Material = part.Material.MaterialString;
         }
 
         private void CommitChanges()
